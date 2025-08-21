a314bd4f62155886c875fc6c3da79b989cd06ce9
Blayne Fuller <befuller2004@gmail.com>
2025-08-20 21:06:42 -0700

    step 4 flipped leds using constraints only

---
+++ Proj1/ee214_proj1_req4/ee214_proj1_req4.srcs/sources_1/new/all_led_reverse.v
@@ -0,0 +1,12 @@
+`timescale 1ns / 1ps
+
+module all_led_reverse(
+    output [9:0] led,
+    output [1:0] RGB_green,
+    input [11:0] sw
+    );
+    
+    assign led = sw[9:0];
+    assign RGB_green = sw[11:10];
+    
+endmodule
